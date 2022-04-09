ce4f1890a35da0126ae45c9d920364ced6587103
tmd22121999 <tmdtmd22121999@gmail.com>
2022-04-09 20:41:23 +0700

    update game landing page

--- src/containers/GameLandingPage/components/Transporter/index.js
+++ src/containers/GameLandingPage/components/Transporter/index.js
@@ -1,12 +1,35 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 function Transporter() {
+  const [progress, setProgress] = useState(0);
+  function updateProgressIndicator() {
+    const windowHeight = window.innerHeight;
+    // const documentFullHeight = document.body.clientHeight;
+
+    const scrolled = window.scrollY;
+
+    const percentageScrolled = (scrolled / (300 + 700 - windowHeight)) * 100;
+
+    setProgress(percentageScrolled);
+  }
+  useEffect(() => {
+    updateProgressIndicator();
+
+    window.addEventListener('scroll', updateProgressIndicator);
+
+    return () => {
+      window.removeEventListener('scroll', updateProgressIndicator);
+    };
+  }, [progress]);
   return (
     <div className="Transporter">
-      <div style={{ backgroundImage: 'url(/wouldyourather/street-sheet.png)' }} className="bgGame" />
-      <div style={{ backgroundImage: 'url(/wouldyourather/grabBikeSheet.png)' }} className="xemay">xemay</div>
+      <h1>Game The Transporter</h1>
+      <div style={{ backgroundImage: 'url(/wouldyourather/street-sheet.png)' }} className="bgGame">
+        <div style={{ backgroundImage: 'url(/wouldyourather/grabBikeSheet2.png)', left: `${progress}vw` }} className="xemay">xemay</div>
+      </div>
+      <p>{progress}</p>
     </div>
   );
 }
